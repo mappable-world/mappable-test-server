c7a786987cc42acda114ba2dfe2cbd00b03af8d5
xdan <chupurnov@gmail.com>
2023-08-20 00:58:03 +0300

    Smoke test

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,29 +4,32 @@ import * as Boom from '@hapi/boom';
 import {logger} from './lib/logger';
 import {asyncMiddleware} from './lib/async-middlware';
 import {loadByBBox} from './middleware/load-by-bbox';
-import {DbDataProvider} from './data-provider/db-data-provider/db-data-provider';
 import {loadByTile} from './middleware/load-by-tile';
+import {DataProvider} from "./data-provider/interface";
 
-const dataProvider = new DbDataProvider();
-export const app = express()
-    .disable('x-powered-by')
-    .disable('etag')
-    .use(express.json())
-    .get('/ping', asyncMiddleware(pingMiddleware.bind(null, dataProvider)))
-    .post('/v1/bbox', asyncMiddleware(loadByBBox.bind(null, dataProvider)))
-    .post('/v1/tile', asyncMiddleware(loadByTile.bind(null, dataProvider)))
-    .use((req: express.Request, res: express.Response, next: express.NextFunction) =>
-        next(Boom.notFound('Endpoint not found'))
-    )
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    .use((err: Error, req: express.Request, res: express.Response, next: unknown) => {
-        if (Boom.isBoom(err)) {
-            sendError(res, err);
-        } else {
-            logger.error('app', err.stack || err);
-            sendError(res, Boom.internal());
-        }
-    });
+export function createApp(dataProvider: DataProvider) {
+    return (
+        express()
+            .disable('x-powered-by')
+            .disable('etag')
+            .use(express.json())
+            .get('/ping', asyncMiddleware(pingMiddleware.bind(null, dataProvider)))
+            .post('/v1/bbox', asyncMiddleware(loadByBBox.bind(null, dataProvider)))
+            .post('/v1/tile', asyncMiddleware(loadByTile.bind(null, dataProvider)))
+            .use((req: express.Request, res: express.Response, next: express.NextFunction) =>
+                next(Boom.notFound('Endpoint not found'))
+            )
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            .use((err: Error, req: express.Request, res: express.Response, next: unknown) => {
+                if (Boom.isBoom(err)) {
+                    sendError(res, err);
+                } else {
+                    logger.error('app', err.stack || err);
+                    sendError(res, Boom.internal());
+                }
+            })
+    );
+}
 
 function sendError(res: express.Response, err: Boom.Boom): void {
     res.status(err.output.statusCode).json(err.output.payload);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import {app} from "./app/app";
+import {createApp} from "./app/app";
 import {logger} from "./app/lib/logger";
 import dotenv from 'dotenv';
+import {DbDataProvider} from "./app/data-provider/db-data-provider/db-data-provider";
 
 dotenv.config();
+const dataProvider = new DbDataProvider();
 
 const port = process.env.MAPS_NODEJS_PORT || 8080;
-app.listen(port, () => {
+createApp(dataProvider).listen(port, () => {
     logger.info('', `App has started at http://localhost:${port}`);
 });
