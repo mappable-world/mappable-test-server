abedb3c0ad445c3e2792111f0dbb910f4af7d42c
xdan <chupurnov@gmail.com>
2023-08-22 19:27:18 +0300

    Release in heroku by CI

--- README.md
+++ README.md
@@ -1,5 +1,29 @@
 # Test server for returning data by tiles and bbox
 
-[Demo](https://mappable-test-server-d7778c5d7460.herokuapp.com/v1/api_docs/)
+[API&Demo](https://mappable-test-server-d7778c5d7460.herokuapp.com/v1/api_docs/)
+
+The test server demonstrates work with subloading data by tiles and by bbox (lower left, upper right points).
+
+You can use to start [docker-compose](https://docs.docker.com/compose/).
+
+```sh
+curl -X 'POST' \
+  'https://mappable-test-server-d7778c5d7460.herokuapp.com/v1/bbox' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "leftBottom": [
+    24,
+    54
+  ],
+  "rightTop": [
+    24,
+    54
+  ]
+}'
+```
+
+
+
 
 
--- src/tools/geojson-to-table-points-sql.ts
+++ src/tools/geojson-to-table-points-sql.ts
@@ -3,7 +3,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as process from 'process';
 
-// ts-node ./src/tools/geojson-to-table-points-sql.ts compose/data/ne_10m_admin_1_label_points.geojson compose/data/dump/points.sql
+// ts-node ./src/tools/geojson-to-table-points-sql.ts src/tests/data/ne_10m_admin_1_label_points.json compose/data/dump/points.sql
 
 const file = path.resolve(process.cwd(), process.argv[2]);
 const output = path.resolve(process.cwd(), process.argv[3]);
@@ -21,7 +21,8 @@ while (data.features.length) {
     const points = data.features.splice(0, 1000);
     const sql = `INSERT INTO public.points (coordinates, json) VALUES
 ${points
-    .map((f: Feature) => `\t('(${f.geometry.coordinates})', '${JSON.stringify(f).replace(/'/g, "''")}')`)
+    // Replace LatLng to LngLat and use PostGis format for point
+    .map((f: Feature) => `\t('POINT(${f.geometry.coordinates.join(' ')})', '${JSON.stringify(f).replace(/'/g, "''")}')`)
     .join(',\n')};`;
 
     inserts.push(sql);
