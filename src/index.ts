aa04a6d9cd475786c6fe505c6f0521a8ddd16213
xdan <chupurnov@gmail.com>
2023-08-22 17:23:23 +0300

    Release in heroku

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-import {createApp} from "./app/app";
-import {logger} from "./app/lib/logger";
+import {createApp} from './app/app';
+import {logger} from './app/lib/logger';
 import dotenv from 'dotenv';
-import {DbDataProvider} from "./app/data-provider/db-data-provider/db-data-provider";
+import {DbDataProvider} from './app/data-provider/db-data-provider/db-data-provider';
 
 dotenv.config();
 const dataProvider = new DbDataProvider();
 
-const port = process.env.MAPS_NODEJS_PORT || 8080;
-createApp(dataProvider).listen(port, () => {
-    logger.info('', `App has started at http://localhost:${port}`);
+const port = process.env.PORT ? +process.env.PORT : 8080;
+const host = process.env.HOST ? process.env.HOST : '0.0.0.0';
+createApp(dataProvider).listen(port, host, () => {
+    logger.info(`App has started at http://${host}:${port}`);
 });
