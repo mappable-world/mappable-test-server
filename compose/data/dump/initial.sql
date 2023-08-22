ecf59a98323f205f07a33df00729252e5bdcba98
xdan <chupurnov@gmail.com>
2023-08-22 19:27:18 +0300

    Release in heroku by CI

--- compose/data/dump/initial.sql
+++ compose/data/dump/initial.sql
@@ -4,6 +4,8 @@ ALTER DATABASE api OWNER TO postgres;
 
 \connect api
 
+CREATE EXTENSION IF NOT EXISTS postgis;
+
 CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA public;
 COMMENT ON EXTENSION pgcrypto IS 'cryptographic functions';
 
@@ -14,7 +16,7 @@ SET default_table_access_method = heap;
 DROP TABLE IF EXISTS public.points;
 CREATE TABLE public.points (
     uid uuid DEFAULT public.gen_random_uuid() NOT NULL,
-    coordinates point NOT NULL,
+    coordinates geography(POINT, 4326) NOT NULL,
     json "json"
 );
 
@@ -24,3 +26,4 @@ ALTER TABLE public.points OWNER TO postgres;
 ALTER TABLE ONLY public.points
     ADD CONSTRAINT points_pkey PRIMARY KEY (uid);
 
+CREATE INDEX global_points_gix ON points USING GIST ( coordinates );
--- src/app/data-provider/db-data-provider/db-data-provider.ts
+++ src/app/data-provider/db-data-provider/db-data-provider.ts
@@ -5,7 +5,7 @@ import {DB} from './pool';
 export class DbDataProvider implements DataProvider {
     async getFeaturesByBBox(bounds: Bounds, counts: number): Promise<Feature[]> {
         const points = await DB.getInstance().query(
-            'select json from public.points where coordinates[0] >= $1 and coordinates[1] <= $2 and coordinates[0] <= $3 and coordinates[1] >= $4 limit $5',
+            'select json from public.points where ST_MakeEnvelope($1, $2, $3, $4) ~ coordinates limit $5',
             [bounds[0][0], bounds[0][1], bounds[1][0], bounds[1][1], counts]
         );
         return points.rows as Feature[];
